1c5041d415c5996f96bde42d1b7a088e39a1106b
bartektroszka <bartek99onet.com.pl@onet.pl>
2020-02-28 19:38:01 +0100

    chess base

--- P1/chess.py
+++ P1/chess.py
@@ -1,5 +1,5 @@
 class Board:
-    def __init__(self, size, whitefigures, blackfigures, whiteking, blackking):
+    def __init__(self, size,whitefigures, blackfigures, whiteking, blackking):
         self.size = size
         self.board = [[] for i in range(self.size)]
         self.whitefigures = whitefigures
@@ -12,14 +12,14 @@ class Board:
 
     def CheckForWhite(self, movex, movey):
         for figure2 in self.whitefigures:
-            if figure2.PosKill((self.blackking.posx + movex), self.blackking.posy + movey):
+            if figure2.PosMove((self.blackking.posx + movex), self.blackking.posy + movey):
                 return True
-        if self.whiteking.PosKill(self.blackking.posx + movex, self.blackking.posy + movey):
+        if self.whiteking.PosMove(self.blackking.posx + movex, self.blackking.posy + movey):
             return True
         return False
 
     def CheckForBlack(self, movex, movey):
-        if self.blackking.PosKill(self.blackking.posx + movex, self.blackking.posy + movey):
+        if self.blackking.PosMove(self.blackking.posx + movex, self.blackking.posy + movey):
             return True
         return False
 
@@ -27,17 +27,17 @@ class Board:
     def CheckMForWhite(self):
         for i in range(3):
             for j in range(3):
-                if (0<=self.blackking.posx + 1-i<self.size and 0<= self.blackking.posy + 1-j<self.size):
-                    if not self.CheckForWhite(1 - i, 1 - j):
+                if (0<=1-i<self.size and 0<=1-j<self.size):
+                    if not self.CheckForWhite(self, 1 - i, 1 - j):
                         return False
         return True
                     
 
 class Figure:
-    def __init__(self, posx, posy, board):
+    def __init__(self, posx, posy, color):
         self.posx = posx
         self.posy = posy
-        self.board = board
+        self.color = color
 
 class Rook(Figure):
     def PosMove(self, x,y):
@@ -50,20 +50,12 @@ class Rook(Figure):
             self.posx = x
             self.posy = y
         
-    def PosKill(self, x, y):
-        if (x == self.posx or y == self.posy) and (x != self.posx or y != self.posy):
-            return True
-        return False
-
+    
 class Blackking(Figure):
     def PosMove(self, x, y):
         if abs(self.posx - x) <= 1 and abs(self.posy - y) <=1:
             if not self.board.CheckForWhite(x - self.posx, y - self.posy):
                 return True
-        return False
-    def PosKill(self, x, y):
-        if 0<abs(self.posx - x) + abs(self.posy - y)<3:
-            return True
         return False
 
     def Move(self, x,y):
@@ -78,24 +70,7 @@ class Whiteking(Figure):
                 return True
         return False
 
-    def PosKill(self, x, y):
-        if 0<abs(self.posx - x) + abs(self.posy - y)<3:
-            return True
-        return False
-
     def Move(self, x,y):
         if self.PosMove(x,y):
             self.posx = x
             self.posy = y
-
-
-board = Board(8, [], [], [], [])
-rook = Rook(1,7,board)
-whiteking = Whiteking(6,5,board)
-blackking = Blackking(7,7,board)
-board.whitefigures = [rook]
-board.whiteking = whiteking
-board.blackking = blackking
-print(board.board)
-print(board.CheckForWhite(0,0))
-print(board.CheckMForWhite())
---
+++ P1/main.py
@@ -0,0 +1,4 @@
+from chess import *
+
+def BST(board):
+    
